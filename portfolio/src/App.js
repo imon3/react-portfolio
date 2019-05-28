c9222744cef824cf0572b32931e0283f5cb75de1
Imon Ovbude <imonovbude@gmail.com>
2019-05-28 13:13:37 -0700

    added background

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "./App.css";
+// mdl import
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <div style={{ height: "300px", position: "relative" }}>
+        <Layout>
+          <Header transparent title="Title" style={{ color: "#fff" }}>
+            <Navigation>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+              <a className="link" href="/">
+                Link
+              </a>
+            </Navigation>
+          </Drawer>
+          <Content />
+        </Layout>
+      </div>
+    </div>
+  );
 }
 
 export default App;
